d9d5addff5d19d99214405c625c1bd3eaa1a1f8d
Lee R. Boynton <lee@boynton.com>
2015-09-12 18:39:46 -0700

    Added missing decompiler case for vector instruction

--- code.go
+++ code.go
@@ -261,6 +261,9 @@ func (code *lcode) decompileInto(buf *bytes.Buffer, indent string, pretty bool) 
 			//fmt.Printf("%sL%03d:\t(jump %d)    \t; L%03d\n", indent, offset, code.ops[offset+1], code.ops[offset+1] + offset)
 			buf.WriteString(begin + "(jump " + strconv.Itoa(code.ops[offset+1]) + ")")
 			offset += 2
+		case opcodeVector:
+			buf.WriteString(begin + "(vector " + strconv.Itoa(code.ops[offset+1]) + ")")
+			offset += 2			
 		case opcodeUse:
 			buf.WriteString(begin + "(use " + code.mod.constants[code.ops[offset+1]].String() + ")")
 			offset += 2
