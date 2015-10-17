9866c9e7eb57e1928e6f2c7d9d222e4f9733ad2e
Lee R. Boynton <lee@boynton.com>
2015-10-16 18:16:37 -0700

    implemented midi input handling
    and consolidated the time stuff to be more general.

--- midi.go
+++ midi.go
@@ -7,43 +7,99 @@ import (
 )
 
 func initMidi() {
-	defineFunction("midi-open", midiOpen, NullType)
-	defineFunction("midi-time", midiTime, NumberType)
-	defineFunction("midi-sleep", midiSleep, NumberType, NumberType)
+   defineFunctionKeyArgs("midi-open", midiOpen, NullType,
+		[]*LOB{StringType, StringType, NumberType},
+		[]*LOB{EmptyString,EmptyString,newInt(1024)},
+		[]*LOB{intern("input:"), intern("output:"), intern("bufsize")})
 	defineFunction("midi-write", midiWrite, NullType, NumberType, NumberType, NumberType)
 	defineFunction("midi-close", midiClose, NullType)
+	defineFunction("midi-listen", midiListen, ChannelType)
 }
 
+var midiOpened = false
+var midiInDevice string
+var midiOutDevice string
+var midiBufsize int64
+var midiBaseTime float64
+
 var midiOut *portmidi.Stream
+var midiIn *portmidi.Stream
+var midiChannel chan portmidi.Event
 var midiMutex = &sync.Mutex{}
 
+func findMidiInputDevice(name string) portmidi.DeviceId {
+	devcount := portmidi.CountDevices()
+	for i := 0; i < devcount; i++ {
+		id := portmidi.DeviceId(i)
+		info := portmidi.GetDeviceInfo(id)
+		if info.IsInputAvailable {
+			if info.Name == name {
+				return id
+			}
+		}
+	}
+	return portmidi.DeviceId(-1)
+}
+
+func findMidiOutputDevice(name string) (portmidi.DeviceId, string) {
+	devcount := portmidi.CountDevices()
+	for i := 0; i < devcount; i++ {
+		id := portmidi.DeviceId(i)
+		info := portmidi.GetDeviceInfo(id)
+		if info.IsOutputAvailable {
+			if info.Name == name {
+				return id, info.Name
+			}
+		}
+	}
+	id := portmidi.GetDefaultOutputDeviceId()
+	info := portmidi.GetDeviceInfo(id)
+	return id, info.Name
+}
+
 func midiOpen(argv []*LOB) (*LOB, error) {
-	latency := int64(0)
-	bufsize := int64(1024)
-	if midiOut == nil {
+//	defaultInput := "USB Oxygen 8 v2"
+//	defaultOutput := "IAC Driver Bus 1"
+	latency := int64(10)
+	if !midiOpened {
 		err := portmidi.Initialize()
 		if err != nil {
 			return nil, err
 		}
-		deviceID := portmidi.GetDefaultOutputDeviceId()
-		out, err := portmidi.NewOutputStream(deviceID, bufsize, latency)
+		midiOpened = true
+		midiInDevice = argv[0].text
+		midiOutDevice = argv[1].text
+		midiBufsize = int64(argv[2].fval)
+
+		outdev, outname := findMidiOutputDevice(midiOutDevice)
+		out, err := portmidi.NewOutputStream(outdev, midiBufsize, latency)
 		if err != nil {
 			return nil, err
 		}
 		midiOut = out
+		midiOutDevice = outname
+		if midiInDevice != "" {
+			indev := findMidiInputDevice(midiInDevice)
+			if indev >= 0 {
+				in, err := portmidi.NewInputStream(indev, midiBufsize)
+				if err != nil {
+					return nil, err
+				}
+				midiIn = in
+			}
+		}
+		midiBaseTime = now()
+		
 	}
-	return Null, nil
-}
-
-// (midi-time) -> seconds
-func midiTime(argc []*LOB) (*LOB, error) {
-	n := float64(portmidi.Time())
-	return newFloat64(n / 1000.0), nil
-}
-
-func midiSleep(argv []*LOB) (*LOB, error) {
-	time.Sleep(time.Duration(argv[0].fval*1000.0) * time.Millisecond)
-	return midiTime(nil)
+	result := makeStruct(4)
+	if midiInDevice != "" {
+		put(result, intern("input:"), newString(midiInDevice))
+	}
+	if midiOutDevice != "" {
+		put(result, intern("output:"), newString(midiOutDevice))
+	}
+	put(result, intern("bufsize:"), newInt64(midiBufsize))
+	return result, nil
 }
 
 func midiAllNotesOff() {
@@ -75,3 +131,33 @@ func midiWrite(argv []*LOB) (*LOB, error) {
 	midiMutex.Unlock()
 	return Null, err
 }
+
+func midiListen(argv []*LOB) (*LOB, error) {
+	ch := Null
+	midiMutex.Lock()
+	if midiIn != nil {
+		ch = newChannel(int(midiBufsize), "midi")
+		go func(s *portmidi.Stream, ch *LOB) {
+			for {
+				time.Sleep(10 * time.Millisecond)
+				events, err := s.Read(1024)
+				if err != nil {
+					continue
+				}
+				channel := ch.channel
+				if channel != nil {
+					for _, ev := range events {
+						ts := (float64(ev.Timestamp) / 1000) + midiBaseTime
+						st := ev.Status
+						d1 := ev.Data1
+						d2 := ev.Data2
+						channel <- list(newFloat64(ts), newInt64(st), newInt64(d1), newInt64(d2))
+					}
+				}
+			}
+		}(midiIn, ch)
+	}
+	midiMutex.Unlock()
+	return ch, nil
+}
+
